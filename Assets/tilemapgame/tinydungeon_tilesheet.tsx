5d31f939c46fa13a07ba88542aa8c239158e3e10
matthewxuereb06 <matthew.xuereb@students.mq.edu.au>
2025-05-06 15:35:09 +1000

    fix??

--- Assets/tilemapgame/tinydungeon_tilesheet.tsx
+++ Assets/tilemapgame/tinydungeon_tilesheet.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tinydungeon_tilesheet" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+<tileset version="1.10" tiledversion="1.11.2" name="tinydungeon_tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <grid orientation="orthogonal" width="32" height="32"/>
  <image source="../tiled/kenney_tiny-dungeon/Tilemap/tilemap.png" width="203" height="186"/>
 </tileset>
